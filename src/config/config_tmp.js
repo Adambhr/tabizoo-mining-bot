eae8dfe701ec2a52c55734d2246121dbd9aa9845
KABOS-Dz <Adambhr143@gmail.com>
2026-03-09 09:36:29 +0100

    Set actual values for Telegram app credentials
    Updated TELEGRAM_APP_ID and TELEGRAM_APP_HASH with actual values.

--- src/config/config_tmp.js
+++ src/config/config_tmp.js
@@ -1,4 +1,4 @@
 export class Config {
-  static TELEGRAM_APP_ID = undefined; // YOUR APP ID EX : 324893724923
-  static TELEGRAM_APP_HASH = undefined; // YOUR APP HASH EX: "aslkfjkalsjflkasf"
+  static TELEGRAM_APP_ID = 27909821; // YOUR APP ID EX : 324893724923
+  static TELEGRAM_APP_HASH = 1d00e401af48779e8c2be61a0bd3a327; // YOUR APP HASH EX: "aslkfjkalsjflkasf"
 }
